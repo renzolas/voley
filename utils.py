0687fdc753a29aa455ea9140c0abc413b42b5c3a
renzolas <144304815+renzolas@users.noreply.github.com>
2025-07-12 18:57:45 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
+# utils.py
+import streamlit as st
+
+def get_available_hours():
+    return [
+        "08:00 - 09:00",
+        "09:00 - 10:00",
+        "10:00 - 11:00",
+        "13:00 - 14:00",
+        "14:00 - 15:00",
+        "17:00 - 18:00",
+        "18:00 - 19:00"
+    ]
+
+def get_status_badge(current, max_):
+    if current >= max_:
+        return "🔴 *Lleno*"
+    else:
+        return "🟢 *Disponible*"
 
