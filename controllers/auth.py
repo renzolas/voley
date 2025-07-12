cf7801e12021ef3a5caf2050bcb9468ba1a4a459
renzolas <144304815+renzolas@users.noreply.github.com>
2025-07-12 17:44:45 -0500

    Delete controllers directory

--- controllers/auth.py
+++
@@ -1,8 +0,0 @@
-from data.db_simulada import registrar_usuario, validar_login
-
-def login(username, password):
-    return validar_login(username, password)
-
-def register(username, password, role):
-    return registrar_usuario(username, password, role)
-
